45b9ff859a044834de8eb51eb1a4ea5d85d1b605
DIWIAtSchool <dietmar.winkler@htl-saalfelden.at>
2024-06-11 16:56:19 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RdrOleDll4_Client_Net_4_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Sunrise");
+            RdrOleDll4.IOleReaderServer readerServer;
+            RdrOleDll4.IOleInstrument instrument;
+
+            readerServer = new RdrOleDll4.ReaderServer();
+            readerServer.InstrumentName = "Sunrise";
+            readerServer.CommType = 1; // 1 == Simulate instrument (this only works, if simulation is available)
+            instrument = readerServer.CreateInstrumentObject();
+            Debug.Assert((instrument as Object) != null);
+
+            Console.WriteLine($"Name={instrument.InstrumentName}");
+            Console.WriteLine($"Serial number={instrument.SerialNumber}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
