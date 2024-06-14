9a33fd0986a3b54dabfb7b1f62837b982b4e8aa0
DIWIAtSchool <dietmar.winkler@htl-saalfelden.at>
2024-06-14 21:12:53 +0200

    Executables added

--- Program.cs
+++ Program.cs
@@ -15,17 +15,22 @@ namespace RdrOleDll4_Client_Net_4_8
             RdrOleDll4.IOleReaderServer readerServer;
             RdrOleDll4.IOleInstrument instrument;
 
-            readerServer = new RdrOleDll4.ReaderServer();
-            readerServer.InstrumentName = "Sunrise";
-            readerServer.CommType = 1; // 1 == Simulate instrument (this only works, if simulation is available)
-            instrument = readerServer.CreateInstrumentObject();
-            Debug.Assert((instrument as Object) != null);
+            try
+            {
+                readerServer = new RdrOleDll4.ReaderServer();
+                readerServer.InstrumentName = "Sunrise";
+                readerServer.CommType = 0; // 1 == Simulate instrument (this only works, if simulation is available)
+                instrument = readerServer.CreateInstrumentObject();
+                Debug.Assert((instrument as Object) != null);
 
-            Console.WriteLine($"Name={instrument.InstrumentName}");
-            Console.WriteLine($"Serial number={instrument.SerialNumber}");
-
+                Console.WriteLine($"Name={instrument.InstrumentName}");
+                Console.WriteLine($"Serial number={instrument.SerialNumber}");
+            } 
+            catch (Exception ex) 
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
             Console.ReadLine();
-
         }
     }
 }
